2b766ae79c1b71d7171abb5a548461474d31ddce
Zendy <nguyenduc.051194@gmail.com>
2021-09-19 22:14:38 +0700

    Appbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:furniture/constants.dart';
+import 'package:furniture/screens/home/components/body.dart';
+import 'package:furniture/size_config.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    // It help us to make our UI responsive
+    SizeConfig().init(context);
+
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      leading: IconButton(
+        onPressed: () {},
+        icon: SvgPicture.asset(
+          "assets/icons/menu.svg",
+          height: SizeConfig.defaultSize! * 2, // 20
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            "assets/icons/scan.svg",
+            height: SizeConfig.defaultSize! * 2.4,
+          ),
+        ),
+        Center(
+          child: Text(
+            "Scan",
+            style: TextStyle(color: kTextColor, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(width: SizeConfig.defaultSize!)
+      ],
+    );
   }
 }
